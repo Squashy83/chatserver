730e32e30ca7c3ba13007a98d7d79a249f73b701
Simone DELLA MARCA <simone.dellamarca@altran.it>
2019-11-09 19:56:48 +0100

    removed dist

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-server_1.default.listen(process.env.PORT, function () {
-    console.log(`Server listening on port ${process.env.PORT}`);
-});
-//# sourceMappingURL=app.js.map
